c7c48d2a7faf7d51fdb794c6f80ccdd64a6d32f2
Naruto Ishikawa <garypippi@gmail.com>
2022-06-05 20:58:14 +0900

    [nvim] Add lualine filename setting

--- nvim/lua/pippi/plug/lualine.lua
+++ nvim/lua/pippi/plug/lualine.lua
@@ -5,10 +5,21 @@ return PippiPlug.new({
         ['nvim-lualine/lualine.nvim'] = {}
     },
     callback = function ()
-        require'lualine'.setup({
-            extensions = {
-                'fern'
+        local lualine = require'lualine'
+        local setting = lualine.get_config()
+        setting.extensions = {'fern'}
+        setting.sections.lualine_c = {{
+            'filename',
+            file_status = true,
+            path = 1, -- relative path
+            shorting_target = 40,
+            symbols = {
+                modified = '[+]',      -- Text to show when the file is modified.
+                readonly = '[-]',      -- Text to show when the file is non-modifiable or readonly.
+                unnamed = '[No Name]', -- Text to show for unnamed buffers.
             }
-        })
+        }}
+
+        lualine.setup(setting)
     end
 })
