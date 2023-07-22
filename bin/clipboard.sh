cfa62ce7d74223e572b4fe3610b6eacf7fbe7e64
Naruto Ishikawa <garypippi@gmail.com>
2023-07-22 20:43:55 +0900

    Add clipboard script

---
+++ bin/clipboard.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Path to clipboard
+file=/tmp/clipboard
+
+# Clear clipboard content
+> $file
+
+if `alacritty --class 'chotto' -e nvim -c 'startinsert' $file`; then
+    head -c -1 $file | wl-copy
+    notify-send Copied.
+fi
